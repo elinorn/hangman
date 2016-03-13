0670e84af7d70adc44ce27279a524d9e06b00ed5
elinorn <elinor@nylanderfamily.org>
2016-03-12 21:56:22 -0800

    more progress

---
+++ words.js
@@ -0,0 +1,25 @@
+var grades = new Object()
+grades.easy= 
+"exit",
+"car",
+"small",
+"gas",
+"no",
+"time");
+
+grades.medium=("magnet",
+"school",
+"road",
+"lion",
+"little",
+"self",
+"creek",
+"house");
+"awe",
+grades.hard=("nutrition",
+"rainbow",
+"beware",
+"apartment",
+"traveling",
+"storage",
+"parking");
